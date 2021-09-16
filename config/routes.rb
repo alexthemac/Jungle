e1a3628a98c6087888a340484b4fbc657a50cf94
Alex MacLure <almaclure@gmail.com>
2021-09-16 17:28:34 +0000

    Add create category page and index page for admin

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+ 
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
